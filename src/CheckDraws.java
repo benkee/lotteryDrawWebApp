e88209cee4f30e703170a8a0a1f89581f856c590
b9033831 <b.keenan11@newcastle.ac.uk>
2020-11-28 19:23:52 +0000

    changed url's to all docker

--- src/CheckDraws.java
+++ src/CheckDraws.java
@@ -29,10 +29,10 @@ public class CheckDraws extends HttpServlet {
         // (NOTE: please change to option 1 when submitting)
 
         // 1. use this when running everything in Docker using docker-compose
-        //String DB_URL = "jdbc:mysql://db:3306/lottery";
+        String DB_URL = "jdbc:mysql://db:3306/lottery";
 
         // 2. use this when running tomcat server locally on your machine and mysql database server in Docker
-        String DB_URL = "jdbc:mysql://localhost:33333/lottery";
+        //String DB_URL = "jdbc:mysql://localhost:33333/lottery";
 
         // 3. use this when running tomcat and mysql database servers on your machine
         //String DB_URL = "jdbc:mysql://localhost:3306/lottery";
